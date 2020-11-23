621b9bd78f8fbedea15b117c6a77b2bd65745aa7
ozicreative <rozbalens@gmail.com>
2020-11-23 11:58:21 +0700

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
 
+$routes->get('/komik/(:segment)', 'Komik::detail/$1');
+
 /**
  * --------------------------------------------------------------------
  * Additional Routing
