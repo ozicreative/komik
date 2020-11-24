1715b9d52952287ce2eb5690eaba1a6b930d11b5
ozicreative <rozbalens@gmail.com>
2020-11-24 19:33:28 +0700

    commit

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
 	/**
 	 * @var string
 	 */
-	public $fromEmail;
+	public $fromEmail = 'choirur15@gmail.com';
 
 	/**
 	 * @var string
 	 */
-	public $fromName;
+	public $fromName = 'AdminKomik';
 
 	/**
 	 * @var string
