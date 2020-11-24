c9b81228ba7ce0d9e8dac53959bb7f2f5c993b51
ozicreative <rozbalens@gmail.com>
2020-11-24 20:17:10 +0700

    commit

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,17 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Logo</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="<?= base_url('/') ?>">Home</a>
-                    <a class="nav-item nav-link" href="<?= base_url('pages/about') ?>">About</a>
-                    <a class="nav-item nav-link" href="<?= base_url('pages/contact') ?>">Contact</a>
-                    <a class="nav-item nav-link" href="<?= base_url('komik') ?>">Komik</a>
-                    <a class="nav-item nav-link" href="<?= base_url('orang') ?>">Orang</a>
-                </div>
+    <div class="container">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="<?= base_url('/') ?>">Home</a>
+                <a class="nav-item nav-link" href="<?= base_url('pages/about') ?>">About</a>
+                <a class="nav-item nav-link" href="<?= base_url('pages/contact') ?>">Contact</a>
+                <a class="nav-item nav-link" href="<?= base_url('komik') ?>">Komik</a>
+                <a class="nav-item nav-link" href="<?= base_url('orang') ?>">Orang</a>
             </div>
+            <?php if (logged_in()) : ?>
+                <a class="nav-item nav-link" href="/logout">Logout</a>
+            <?php else : ?>
+                <a class="nav-item nav-link" href="/logout">Login</a>
+            <?php endif; ?>
         </div>
-    </nav>
+    </div>
+</nav>
